4b363a944dc7ac6d838a838614a32e575b86d50d
Demo User <demouser@MacBook-Pro.local>
2016-08-01 16:14:25 -0400

    Added files

--- main.py
+++ main.py
@@ -1,25 +1,68 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import jinja2
+import os
 import webapp2
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class prof_form(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    birthdate = ndb.StringProperty(required=True)
+    phone = ndb.StringProperty(required=True)
+    city = ndb.StringProperty(required=True)
+    genre = ndb.StringProperty(required=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #get function reads the file and puts it into the template
+        template = jinja_environment.get_template('templates/prof_form.html')
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
+        self.response.out.write('%s' % greeting)
+        #sends it to the client
+        self.response.write(template.render())
+
+    def post(self):
+        template = jinja_environment.get_template('templates/profout_order.html')
+        name_value = self.request.get('name')
+        email_value = self.request.get('email')
+        birthdate_value = self.request.get('birthdate')
+        phone_value = self.request.get('phone')
+        city_value = self.request.get('city')
+        genre_value = self.request.get('genre')
+        #prepares data for the template
+        prof_form = {
+          'name_answer': name_value,
+          'email_answer': email_value,
+          'birthdate_answer': birthdate_value,
+          'phone_answer' phone_value,
+          'city_answer': city_value,
+          'genre_answer': genre_value,
+          }
+        prof_record = Form(
+            name = name_value,
+            email = email_value,
+            birthdate = birthdate_value,
+            phone = phone_value,
+            city = city_value,
+            genre = genre_value,
+          )
+        key = prof_record.put()
+        #generates final html page
+        html_page = template.render(prof_form)
+        # and sends the response
+        self.response.write(html_page)
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+  ('/', MainHandler),
 ], debug=True)
